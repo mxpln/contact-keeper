3e454c0633626596b82eb8470e15430f243338e8
Maxime Paolone <paolonemax@gmail.com>
2020-12-30 18:23:41 +0100

    Replaced config with dotenv

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
-
+const connectDB = require("./config/db");
 const app = express();
+
+connectDB();
 app.get("/", (req, res) => res.json({ msg: "ContactKeeper API" }));
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
